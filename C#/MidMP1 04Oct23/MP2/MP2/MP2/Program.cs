11b894a99f0729009bb5be6c558f112eff196b21
Agayessh <164612348+Agayessh@users.noreply.github.com>
2025-11-30 00:04:50 +0800

    Add files via upload

---
+++ C#/MidMP1 04Oct23/MP2/MP2/MP2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num < 40)
+            {
+                Console.WriteLine("First 20 natural numbers: " + num);
+                num += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
