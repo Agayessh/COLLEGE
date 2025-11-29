213166c8edde99a03a2fd8982cb2ec10782e0d93
Agayessh <164612348+Agayessh@users.noreply.github.com>
2025-11-29 23:36:03 +0800

    Delete 1st/MidMP2 11Oct23 directory

--- 1st/MidMP2 11Oct23/README.d
+++
@@ -1,4 +0,0 @@
-# Consists of 3 programs:
-# Create a program using arrays that will accept 15 numbers, then list the odd numbers and even numbers.
-# Create a program that can accept 10 names, then display them in reverse order.
-# Create a program using loops that will display the multiplication table (table of 10)
